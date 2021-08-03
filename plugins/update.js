56f98fd36c40d3692ca3cd1f8febbd87252f7757
MouadElJanati <81334062+MouadElJanati@users.noreply.github.com>
2021-08-03 12:07:34 +0100

    Update update.js

--- plugins/update.js
+++ plugins/update.js
@@ -1,7 +1,7 @@
 let { execSync } = require('child_process')
 let handler = async (m, { conn, text, isROwner }) => {
   if (global.conn.user.jid == conn.user.jid) {
-    let stdout = execSync('git remote set-url origin https://github.com/Nurutomo/wabot-aq.git && git pull' + (isROwner && text ? ' ' + text : ''))
+    let stdout = execSync('git remote set-url origin https://github.com/MouadElJanati/oussama.git && git pull' + (isROwner && text ? ' ' + text : ''))
     if (isROwner) require('fs').readdirSync('plugins').map(v => global.reload('', v))
     m.reply(stdout.toString())
   }
@@ -10,7 +10,7 @@ handler.help = ['update']
 handler.tags = ['host']
 handler.command = /^update$/i
 handler.owner = false
-handler.mods = false
+handler.mods = true
 handler.premium = false
 handler.group = false
 handler.private = false

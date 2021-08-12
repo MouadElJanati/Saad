7028125a47113440f3f6316d00f4f8e16d190624
MouadElJanati <81334062+MouadElJanati@users.noreply.github.com>
2021-08-12 23:55:22 +0100

    Update Uq.js

--- plugins/Uq.js
+++ plugins/Uq.js
@@ -1,5 +1,5 @@
 const free = 50000000
-const prem = 5000
+const prem = 50000000
 let handler = async (m, { isPrems }) => {
   let time = global.db.data.users[m.sender].lastclaim + 86400000
   if (new Date - global.db.data.users[m.sender].lastclaim < 86400000) throw `Anda sudah mengklaim klaim harian hari ini\ntunggu selama ${msToTime(time - new Date())} lagi`
